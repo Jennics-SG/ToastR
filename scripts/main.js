78b4f20f77949a3518f5093ca80c5ce2ad8a01e1
Jimy <jimyhoulbrook@gmail.com>
2021-07-16 17:58:07 +0100

    awd
    awd

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,18 @@
-const _gameElems = [];
+const breadInteracts = [];
+const spreads = [];
 
 // we use custom classes to allow for custom variables for the object
 class breadObj extends PIXI.Sprite {
-    constructor(x, y, texture){
+    constructor(x = 0, y = 0, texture){
         super(texture);
         this.anchor.set(0.5);
         this.x = x;
         this.y = y;
+        this.scale.set(0.8);
         this.state = 1;
         this.interactive = true;
         this.zIndex = 1;
+        this.dragging = true;
     }
 
     // Dragging physics and code
@@ -26,13 +29,17 @@ class breadObj extends PIXI.Sprite {
         }
     }
 
-    dragEnd(elems){
-        this.dragging = false;
+    dragEnd(elems, spreads){
+        let spreadsInteractive = () => {for(const elem of spreads) elem[1].interactive = true}
 
-        // Checks to see what game elements should be interactive
+        this.dragging = false;
         for(const elem of elems){
-            if(elem.containsPoint(this)) elem.interactive = true;
-            else elem.interactive = false;
+            if(elem[0] == "choppingBoard" && elem[1].containsPoint(this)){
+                spreadsInteractive()
+                break;
+            }
+            else if (elem[1].containsPoint(this)) {elem[1].interactive = true;}
+            else {elem.interactive = false;}
         }
     }
 
@@ -51,7 +58,6 @@ class breadObj extends PIXI.Sprite {
         await this.changeTexture(loader, toaster.setting);
 
         // Tween the toast popping out of the toaster here
-        console.log('toasting done');
         await Promise.all([this.finToastingBread(this, toaster.y - 250), this.finToastingLever(lever, toaster.y - 50)]);
         toaster.texture = loader.resources['toaster_up'].texture;
         this.interactive = true;
@@ -71,10 +77,7 @@ class breadObj extends PIXI.Sprite {
         });
     }
 
-    getRid(){
-        this.bread ;
-        this.destroy();
-    }
+    getRid(){this.destroy();}
 
     startToastingAnims(elem, newPos){
         return new Promise((resolve) => {
@@ -120,6 +123,7 @@ class toasterObj extends PIXI.Sprite{
         this.anchor.set(0.5);
         this.x = x;
         this.y = y;
+        this.scale.set(0.8);
 
         // toasterObj.setting will be set by a dial on the toaster. for now default value is 5
         this.setting = 1;
@@ -132,31 +136,6 @@ class toasterObj extends PIXI.Sprite{
     }
 }
 
-class loafObj extends PIXI.Sprite{
-    constructor(x, y, texture){
-        super(texture);
-        this.x = x;
-        this.y = y;
-        this.anchor.set(1);
-        this.interactive = true;
-        this.usable = true;
-    }
-
-    onClick(container, loader, gameElems, e){
-        if(this.usable){
-            this.bread = new breadObj(e.data.global.x, e.data.global.y, loader.resources['bread1'].texture);
-            this.bread.mousedown = this.bread.dragStart;
-            this.bread.mousemove = this.bread.dragMove;
-            this.bread.mouseup = () => this.bread.dragEnd(gameElems);
-            container.addChild(this.bread);
-            this.usable = false;
-        } else {
-            this.usable = true;
-            this.bread.getRid();
-        }
-    }
-}
-
 function init(){
     this.app = new PIXI.Application({
         height: 1080,
@@ -165,21 +144,26 @@ function init(){
     });
     document.body.appendChild(this.app.view);
 
-    const _files = [['bread1', 'bread1.png'],
+    const _files = [['background', 'background.png'],
+                    ['bread1', 'bread1.png'],
                     ['bread2', 'bread2.png'],
                     ['bread3', 'bread3.png'],
                     ['bread4', 'bread4.png'],
                     ['bread5', 'bread5.png'],
                     ['bread6', 'bread6.png'],
+                    ['butter', 'butter.png'],
+                    ['choppingBoard', 'chopping_board.png'],
                     ['dial1', 'dial_1.png'],
                     ['dial2', 'dial_2.png'],
                     ['dial3', 'dial_3.png'],
                     ['dial4', 'dial_4.png'],
                     ['dial5', 'dial_5.png'],
-                    ['toaster_up', 'toaster.png'],
+                    ['loaf', 'loaf.png'],
+                    ['nut', 'nut.png'],
+                    ['orderTV', 'order_tv.png'],
                     ['toaster_down', 'toaster_down.png'],
                     ['lever', 'toaster_lever.png'],
-                    ['loaf', 'loaf.png']];
+                    ['toaster_up', 'toaster.png']];
 
     this.loader = new PIXI.Loader("../assets");
     for(const file of _files){this.loader.add(file[0], file[1]);}
@@ -194,41 +178,78 @@ function init(){
 function onReady(){
     console.log('Game Initialised');
 
+    const background = new PIXI.Sprite.from(this.loader.resources.background.texture);
+    background.zIndex = 0;
+    this.app.stage.addChild(background);
+
+    const orderTV = new PIXI.Sprite.from(this.loader.resources.orderTV.texture);
+    orderTV.position.set(orderTV.width / 4, orderTV.height / 4);
+    this.app.stage.addChild(orderTV);
+
     const game = new PIXI.Container()
     game.sortableChildren = true;
     this.app.stage.addChild(game);
 
     const toaster = new toasterObj(250, this.app.view.height / 1.25, this.loader.resources.toaster_up.texture);
-    toaster.zIndex = 2;
+    toaster.zIndex = 3;
     game.addChild(toaster);
-    _gameElems.push(toaster);
+    breadInteracts.push(['toaster', toaster]);
+
+    const loaf = new PIXI.Sprite.from(this.loader.resources.loaf.texture);
+    loaf.scale.set(0.8);
+    loaf.position.set((this.app.view.width - (loaf.width * 2)) / 2, (this.app.view.height - (loaf.height - 30)) / 2);
+    loaf.interactive = true;
+    loaf.mousedown = (e) => this.bread = makeBread.bind(this)(game, loaf, breadInteracts, spreads, e);
+    game.addChild(loaf);
+
+    const choppingBoard = new PIXI.Sprite.from(this.loader.resources.choppingBoard.texture);
+    choppingBoard.position.set((this.app.view.width / 2) + 100, this.app.view.height / 1.4);
+    game.addChild(choppingBoard);
+    breadInteracts.push(['choppingBoard', choppingBoard]);
+
+    const butter = new PIXI.Sprite.from(this.loader.resources.butter.texture);
+    butter.position.set(choppingBoard.x, choppingBoard.y - (butter.height * 1.1));
+    game.addChild(butter);
+    spreads.push(['butter', butter]);
+
+    const nut = new PIXI.Sprite.from(this.loader.resources.nut.texture);
+    nut.position.set(butter.x * 1.2, butter.y);
+    game.addChild(nut);
+    spreads.push(['nut', nut])
 
     const dial = new PIXI.Sprite.from(this.loader.resources['dial1'].texture);
     dial.anchor.set(0.5);
     dial.x = toaster.x - 10;
     dial.y = toaster.y + 50;
-    dial.zIndex = 3;
+    dial.zIndex = 4;
     dial.interactive = true;
     dial.mousedown = () => toaster.changeSetting(dial, this.loader);
     game.addChild(dial);
 
     const lever = new PIXI.Sprite.from(this.loader.resources['lever'].texture);
-    lever.x = toaster.x + 225;
+    lever.x = toaster.x + 170;
     lever.y = toaster.y - 50;
     game.addChild(lever);
-
-    const loaf = new loafObj(this.app.view.width, 500, this.loader.resources['loaf'].texture);
-    loaf.mousedown = (e) => loaf.onClick(game, this.loader, _gameElems, e);
-    game.addChild(loaf);
 
     toaster.mousedown = () => this.bread.makeToast(this.loader, toaster, lever);
 }
 
-function animate(){TWEEN.update(this.app.ticker.lastTime);}
-
-function makeBread(container){
-    const bread = new breadObj(10, 10, this.loader.resources['bread1'].texture);
-    container.addChild(bread);
+function makeBread(container, loaf, breadElems, spreads, e){
+    if(this.bread == undefined){
+        const bread = new breadObj(e.data.global.x, e.data.global.y, this.loader.resources['bread1'].texture);
+        bread.mousedown = bread.dragStart;
+        bread.mousemove = bread.dragMove;
+        bread.mouseup = () => bread.dragEnd(breadElems, spreads)
+        container.addChild(bread);
+        loaf.usable = false;
+        return bread;
+    } else {
+        // Dev tool, DELETE
+        this.bread.getRid();
+        loaf.usable = true;
+    }
 }
+
+function animate(){TWEEN.update(this.app.ticker.lastTime);}
 
 document.addEventListener('DOMContentLoaded', init);
