3599d3e7b41011e2a944d9d94f4f5bc543f01eb4
Jimy <jimyhoulbrook@gmail.com>
2025-08-13 19:05:31 +0100

    ascii

--- totos/database.js
+++ totos/database.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const colours = require('../consoleColours');
 
 module.exports.connect = () => {
-    // Set up default conn
-    const mongoDB = "mongodb+srv://jennics:ToastR-password@cluster0.yaepyc6.mongodb.net/ToastR-Scoreboard"
-    mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+    // // Set up default conn
+    // const mongoDB = "mongodb+srv://jennics:ToastR-password@cluster0.yaepyc6.mongodb.net/ToastR-Scoreboard"
+    // mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 
-    const db = mongoose.connection;
+    // const db = mongoose.connection;
 
-    db.on('error', console.error.bind(console, "MongoDB Connection Error:"));
-    db.once('open', () => {
-        console.log(colours.foregroud.green, "Database Connection Established")
-    });
+    // db.on('error', console.error.bind(console, "MongoDB Connection Error:"));
+    // db.once('open', () => {
+    //     console.log(colours.foregroud.green, "Database Connection Established")
+    // });
 }
